cf44bd311027450bf66e77880470ff7d977dff7d
Sudeesh025 <138246429+Sudeesh025@users.noreply.github.com>
2023-10-17 09:42:06 +0530

    Delete day_8 directory

--- day_8/selection.py
+++
@@ -1,9 +0,0 @@
-a=[12,8,2,19,69]
-for i in range(0,len(a)):
-    min_index=i
-    for j in range(i+1,len(a)):
-        if(a[min_index]>a[j]):
-            t=a[min_index]
-            a[min_index]=a[j]
-            a[j]=t
-print(a)
